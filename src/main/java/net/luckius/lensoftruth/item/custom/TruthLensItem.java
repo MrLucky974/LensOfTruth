43003d8d85cc31706dfb3042cd9aaad0ccdbdcc6
MrLucky974 <HeroCreepyFR@gmail.com>
2022-06-22 18:19:05 +0400

    Added custom class TruthLensItem

---
+++ src/main/java/net/luckius/lensoftruth/item/custom/TruthLensItem.java
@@ -0,0 +1,37 @@
+package net.luckius.lensoftruth.item.custom;
+
+import net.luckius.lensoftruth.Mod;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class TruthLensItem extends Item {
+    public TruthLensItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+
+        Mod.LOGGER.info("using lens of truth");
+
+        return TypedActionResult.success(itemStack);
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return 72000;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+
+
+}
