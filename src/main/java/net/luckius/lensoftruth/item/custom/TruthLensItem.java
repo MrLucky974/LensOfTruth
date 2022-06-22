03b1f44683445d5a87b41f11e82c1db6f04340f2
MrLucky974 <HeroCreepyFR@gmail.com>
2022-06-22 18:33:41 +0400

    Copying shamelessly the spyglass item for the TruthLensItem

--- src/main/java/net/luckius/lensoftruth/item/custom/TruthLensItem.java
+++ src/main/java/net/luckius/lensoftruth/item/custom/TruthLensItem.java
@@ -1,35 +1,51 @@
 package net.luckius.lensoftruth.item.custom;
 
 import net.luckius.lensoftruth.Mod;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
+/// TODO : display invisible entities when using the item
+/// TODO : add the effect on the screen when using the item
 public class TruthLensItem extends Item {
+    public static final int MAX_USE_TIME = 1200;
+
     public TruthLensItem(Settings settings) {
         super(settings);
     }
 
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-
-        Mod.LOGGER.info("using lens of truth");
-
-        return TypedActionResult.success(itemStack);
-    }
-
     public int getMaxUseTime(ItemStack stack) {
-        return 72000;
+        return MAX_USE_TIME;
     }
 
     public UseAction getUseAction(ItemStack stack) {
-        return UseAction.BOW;
+        return UseAction.SPYGLASS;
     }
 
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.ITEM_SPYGLASS_USE, 1.0F, 1.0F);
 
+        return ItemUsage.consumeHeldItem(world, user, hand);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        this.playStopUsingSound(user);
+        return stack;
+    }
+
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        this.playStopUsingSound(user);
+    }
+
+    private void playStopUsingSound(LivingEntity user) {
+        user.playSound(SoundEvents.ITEM_SPYGLASS_STOP_USING, 1.0F, 1.0F);
+    }
 }
